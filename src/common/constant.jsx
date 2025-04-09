59d304eaa4e893cc548de9829aa8aa6e0be3590f
riya <riya@samyotech.com>
2025-04-09 18:23:00 +0530

    added code for testing

--- src/common/constant.jsx
+++ src/common/constant.jsx
@@ -282,5 +282,8 @@ export const text = {
   CURRENCY_SYMBOL:'Currency Symbol',
   PROFILE: 'Profile',
   SETTINGS: 'Settings',
-  LOGOUT: 'Logout'
+  LOGOUT: 'Logout',
+  VIEW:'  View',
+  DRIVER_PHOTO:'Driver Photo',
+  DRIVER_DOC: 'Driver Document'
 };
